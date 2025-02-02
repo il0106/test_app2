f92be19f1ce47fab2bdde40894da0d17315df0e7
ilya_starkov <81004068+il0106@users.noreply.github.com>
2025-02-02 20:09:09 +0300

    -

--- backend/src/users/auth.py
+++ backend/src/users/auth.py
@@ -1,5 +1,9 @@
 
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from src.users.dao import UsersDAO
+from pydantic import EmailStr
+from jose import jwt
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -8,3 +12,19 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=24*60)
+    to_encode.update({"exp": expire})
+    
+    encoded_jwt = jwt.encode(to_encode, "asdlajsdasASDASD", algorithm="HS256")
+    return encoded_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return user
--- backend/src/users/router.py
+++ backend/src/users/router.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response, status
 from src.users.dao import UsersDAO
 from src.users.schemas import SUserAuth
-from src.users.auth import get_password_hash
+from src.users.auth import authenticate_user, create_access_token, get_password_hash
+
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth & Users"]
 )
+
 
 @router.post('/register')
 async def register_user(user_data: SUserAuth):
@@ -17,6 +19,11 @@ async def register_user(user_data: SUserAuth):
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
 
 
-@router.post('/login')
-async def register_user(user_data: SUserAuth):
-    
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(user_data.email, user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("app_access_token", access_token,httponly=True)
+    return access_token

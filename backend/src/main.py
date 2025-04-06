10dfdf49589d8ace6aaa4d311ffa14fdf7a3f21e
ilya_starkov <81004068+il0106@users.noreply.github.com>
2025-04-06 19:27:36 +0300

    all works after rebuild repo structure

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn 
-from src.config import settings
+from config import settings
 # from src.users.router import router as router_users
-print(settings)
 
 
 app = FastAPI()
@@ -33,4 +32,4 @@ def read_config():
 
 
 if __name__=="__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

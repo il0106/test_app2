8bafdd2aed42b4922f06a0c7ff5a692216ac1594
ilya_starkov <81004068+il0106@users.noreply.github.com>
2025-07-26 23:10:55 +0300

    теперь nuxt  в контейнере видит env

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,3 @@
-import { config as dotenvConfig } from 'dotenv'
-import { resolve } from 'path'
-
-// Явно указываем путь до нужного .env-файла
-dotenvConfig({ path: resolve(__dirname, '../.env') })
-
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -33,22 +26,22 @@ export default defineNuxtConfig({
     }
   },
   nitro: {
-    envDir: './',
-    devProxy: {
-      '/api': {
-        target: process.env.BACKEND_API,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    // storage: {
-    //   'cache': {
-    //     driver: 'fs',
-    //     base: './.cache'
-    //   }
-    // },
-    // routeRules: {
-    //   '/**': { cache: { swr: true, maxAge: 5 } } // Кэшировать все страницы на ... секунд
-    // }
+    envDir: './', // при разраюотке envDir: './',
+  //   // devProxy: {
+  //   //   '/api': {
+  //   //     target: process.env.BACKEND_API,
+  //   //     changeOrigin: true,
+  //   //     secure: false,
+  //   //   },
+  //   // },
+  //   // storage: {
+  //   //   'cache': {
+  //   //     driver: 'fs',
+  //   //     base: './.cache'
+  //   //   }
+  //   // },
+  //   // routeRules: {
+  //   //   '/**': { cache: { swr: true, maxAge: 5 } } // Кэшировать все страницы на ... секунд
+  //   // }
   }
 });
